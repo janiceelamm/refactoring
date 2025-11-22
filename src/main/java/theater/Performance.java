d689e058f0ded4e445f435eb8c3ed8404d0369a9
janiceelamm <janicelam0811@gmail.com>
2025-11-22 18:26:57 -0500

    complete Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -5,11 +5,20 @@ package theater;
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
     }
+
+    public final String getPlayID() {
+        return playID;
+    }
+
+    public final int getAudience() {
+        return audience;
+    }
+
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,35 @@
+
 package theater;
 
+/**
+ * Represents a play in the theater system.
+ *
+ * @null.none
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    /**
+     * The name of the play.
+     * @null.none
+     */
+    private String name;
+
+    /**
+     * The type of the play.
+     * @null.none
+     */
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public final String getName() {
+        return name;
+    }
+
+    public final String getType() {
+        return type;
     }
 }
